c5cefaafd780b6475d67b83d381b4dfb5b35a9d3
Marcell Julienne <marcell.julienne>
2019-07-16 14:49:12 +0700

    Initial Commit

---
+++ MVP Framework/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  MVP Framework
+//
+//  Created by Marcell Julienne on 16/07/19.
+//  Copyright © 2019 Marcell Julienne. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imagePlayer: UIImageView!
+    @IBOutlet weak var namePlayer: UILabel!
+    
+}
---
+++ MVP Framework/PlayerModel.swift
@@ -0,0 +1,19 @@
+//
+//  PlayerModel.swift
+//  MVP Framework
+//
+//  Created by Marcell Julienne on 16/07/19.
+//  Copyright © 2019 Marcell Julienne. All rights reserved.
+//
+
+import Foundation
+import
+class PlayerModel {
+    var name: String?
+    var profilePicture: String?
+    var chooseHost: Bool?
+    var hostId: String
+    
+    init( playerName , playerprofilePicture , playerHost , playerId: String ) {
+        
+    }
